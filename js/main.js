8219f03cc6a5579c39606181afcbec5ece6e7352
aymaneldawy <aymaneldawy@yahoo.com>
2021-09-12 17:39:40 -0700

    the last stage

--- js/main.js
+++ js/main.js
@@ -1,121 +1,129 @@
-// Addition value by col
-function addByCol(col) {
-  let inputByCol = document.querySelectorAll(
-    `.check .col:nth-child(${col}) input`
-  );
-  let newArr = [];
-  inputByCol.forEach((input) => newArr.push(+input.value));
-  let result = newArr.reduce((a, b) => a + b);
-  return result;
+function getResultByCol(col) {
+  return document.querySelectorAll(
+    `.check .box-col:nth-of-type(${col}) .active`
+  ).length;
 }
-function getResults() {
-  console.log("Run...");
-  document.querySelector(`.results .col:nth-child(3)`).innerHTML = addByCol(3);
-  document.querySelector(`.results .col:nth-child(5)`).innerHTML = addByCol(5);
-  document.querySelector(`.results .col:nth-child(7)`).innerHTML = addByCol(7);
-  document.querySelector(`.results .col:nth-child(9)`).innerHTML = addByCol(9);
-  document.querySelector(`.results .col:nth-child(11)`).innerHTML = addByCol(
-    11
-  );
-  getFinalResult()
+function rating() {
+  function executeRating(stars) {
+    const starClassActive = "rating__star fa fa-star";
+    const starClassInactive = "rating__star fa fa-star-o";
+    const starsLength = stars.length;
+    let i;
+    stars.map((star) => {
+      star.onclick = () => {
+        i = stars.indexOf(star);
+        if (star.className === starClassInactive) {
+          for (i; i >= 0; --i) stars[i].className = starClassActive + ` active`;
+        } else {
+          for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+        }
+      };
+    });
+  }
+  for (let i = 1; i < 26; i++) {
+    for (let j = 1; j < 7; j++) {
+      let ratingStars = [
+        ...document.querySelectorAll(
+          `.check-${i} .box-col:nth-of-type(${j}) .rating__star`
+        ),
+      ];
+      executeRating(ratingStars);
+    }
+  }
+}
+rating();
+function result() {
+ 
+  document.querySelector(".box-result").innerHTML = `
+  <div class="box-col">
+    <strong>مجموع (ب)</strong>
+    <span>${getResultByCol(1)}</span>
+  </div>
+  <div class="box-col">
+    <strong> 1 مجموع (ح)</strong>
+    <span>${getResultByCol(2)}</span>
+  </div>
+  <div class="box-col">
+    <strong>مجموع (س)</strong>
+    <span>${getResultByCol(3)}</span>
+  </div>
+  <div class="box-col">
+
+    <strong>مجموع (م)</strong>
+    <span>${getResultByCol(4)}</span>
+  </div>
+  <div class="box-col">
+    <strong>مجموع (ح)</strong>
+    <span>${getResultByCol(5)}</span>
+  </div>
+  `;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  let inputs = document.querySelectorAll(` .check-25 input`);
-  inputs.forEach((input) => {
-    input.addEventListener("keyup", getResults);
+  document.querySelector(".show-result").addEventListener("click", result);
+  document.querySelector(".fa-times").addEventListener("click", () => {
+    document.querySelector(".modal").classList.remove("show");
   });
-  document.querySelector(".show-result").addEventListener("click", getResults);
+  document.querySelector(".open-modal").addEventListener("click", () => {
+    getFinalResult();
+    document.querySelector(".modal").classList.add("show");
+  });
 });
 
-//
 function getFinalResult() {
-  let valueColOne = addByCol(3);
-  let valueColTwo = addByCol(5);
-  let valueColThree = addByCol(7);
-  let valueColFour = addByCol(9);
-  let valueColFive = addByCol(11);
-  document.querySelector(".row-body").innerHTML = `
-  <div class="row">
-            <div class="col">البقاء والحياة ب</div>
-            <div class="col">${valueColOne > 101 ? valueColOne : "----"}</div>
-            <div class="col">${
-              valueColOne > 75 && valueColOne <= 100 ? valueColOne : "----"
-            }</div>
-            <div class="col">${
-              valueColOne > 51 && valueColOne <= 75 ? valueColOne : "----"
-            }</div>
-            <div class="col">${
-              valueColOne > 25 && valueColOne <= 51 ? valueColOne : "----"
-            }</div>
-            <div class="col">${
-              valueColOne >= 0 && valueColOne <= 25 ? valueColOne : "----"
-            }</div>
-          </div>
-          <div class="row">
-            <div class="col">الحب والانتماء ح</div>
-            <div class="col">${valueColTwo > 101 ? valueColTwo : "----"}</div>
-            <div class="col">${
-              valueColTwo > 75 && valueColTwo <= 100 ? valueColTwo : "----"
-            }</div>
-            <div class="col">${
-              valueColTwo > 51 && valueColTwo <= 75 ? valueColTwo : "----"
-            }</div>
-            <div class="col">${
-              valueColTwo > 25 && valueColTwo <= 51 ? valueColTwo : "----"
-            }</div>
-            <div class="col">${
-              valueColTwo >= 0 && valueColTwo <= 25 ? valueColTwo : "----"
-            }</div>
-          </div>
-          <div class="row">
-            <div class="col">القوة وتقدير الذات س</div>
-            <div class="col">${valueColThree > 101 ? valueColThree : "----"}</div>
-            <div class="col">${
-              valueColThree > 75 && valueColThree <= 100 ? valueColThree : "----"
-            }</div>
-            <div class="col">${
-              valueColThree > 51 && valueColThree <= 75 ? valueColThree : "----"
-            }</div>
-            <div class="col">${
-              valueColThree > 25 && valueColThree <= 51 ? valueColThree : "----"
-            }</div>
-            <div class="col">${
-              valueColThree >= 0 && valueColThree <= 25 ? valueColThree : "----"
-            }</div>
-          </div>
-          <div class="row">
-            <div class="col">الحرية ح1</div>
-            <div class="col">${valueColFour > 101 ? valueColFour : "----"}</div>
-            <div class="col">${
-              valueColFour > 75 && valueColFour <= 100 ? valueColFour : "----"
-            }</div>
-            <div class="col">${
-              valueColFour > 51 && valueColFour <= 75 ? valueColFour : "----"
-            }</div>
-            <div class="col">${
-              valueColFour > 25 && valueColFour <= 51 ? valueColFour : "----"
-            }</div>
-            <div class="col">${
-              valueColFour >= 0 && valueColFour <= 25 ? valueColFour : "----"
-            }</div>
-          </div>
-          <div class="row">
-            <div class="col">المرح م</div>
-            <div class="col">${valueColFive > 101 ? valueColFive : "----"}</div>
-            <div class="col">${
-              valueColFive > 75 && valueColFive <= 100 ? valueColFive : "----"
-            }</div>
-            <div class="col">${
-              valueColFive > 51 && valueColFive <= 75 ? valueColFive : "----"
-            }</div>
-            <div class="col">${
-              valueColFive > 25 && valueColFive <= 51 ? valueColFive : "----"
-            }</div>
-            <div class="col">${
-              valueColFive >= 0 && valueColFive <= 25 ? valueColFive : "----"
-            }</div>
-          </div> `;
+  function getTextResultByCol(num) {
+    if(num > 101) 
+      return 'القوي';
+    else if(num >= 76 && num < 100)
+      return 'فوق المتوسط';
+    else if(num >= 51 && num < 75)
+      return ' متوسط';
+    else if(num >= 26 && num < 50)
+      return ' دون المتوسط';
+    else if(num >= 0 && num < 25)
+      return ' ضعيف';
+  }
+  let colOne = getResultByCol(1);
+  let colTwo = getResultByCol(2);
+  let colThree = getResultByCol(3);
+  let colFour = getResultByCol(4);
+  let colFive = getResultByCol(5);
+  document.querySelector(".modal .body").innerHTML = `
+  <li>
+  <div class="key">البقاء والحياة  (ب)</div>
+  <div class="val">
+    <span>${getTextResultByCol(colOne)}</span>
+    <span>${colOne}</span>
+  </div>
+</li>
 
+<li>
+  <div class="key">الحرية (ح1)</div>
+  <div class="val">
+    <span>${getTextResultByCol(colTwo)}</span>
+    <span>${colTwo}</span>
+  </div>
+</li>
+<li>
+  <div class="key">القوة وتقدير الذات (س)</div>
+  <div class="val">
+    <span>${getTextResultByCol(colThree)}</span>
+    <span>${colThree}</span>
+  </div>
+</li>
+<li>
+  <div class="key">المرح ( م)</div>
+  <div class="val">
+    <span>${getTextResultByCol(colFour)}</span>
+    <span>${colFour}</span>
+  </div>
+</li>
+<li>
+  <div class="key">الحب والانتماء (ح)</div>
+  <div class="val">
+  <span>${getTextResultByCol(colFive)}</span>
+  <span>${colFive}</span>
+  </div>
+</li>
+`;
 }
-getFinalResult()
